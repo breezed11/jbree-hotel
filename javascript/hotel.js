8de56d6b78f553cb094c865098a5f613edd58977
jamesbree99@gmail.com <jamesbree99@gmail.com>
2020-04-17 23:03:33 +0100

    Login changes

--- javascript/hotel.js
+++ javascript/hotel.js
@@ -1,10 +1,9 @@
 function send_request(url_to_send, data_to_send) {
-    data_to_send = '{"test": "test"}';
     $.ajax(url_to_send, {
         type: 'POST',
         data: {
             request: data_to_send,
-            system_ref : document.getElementById("system_ref").value
+            system_ref: document.getElementById("system_ref").value
         },
         success: function (result) {
             check_for_errors(result);
@@ -16,4 +15,19 @@ function send_request(url_to_send, data_to_send) {
 
 function check_for_errors(data) {
     window.alert(data);
+}
+
+function login() {
+    var url_to_send = 'cgi-bin/hotel.pl';
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("psw").value;
+
+    let data_to_send = {
+        "Authentication": {
+            "username": username,
+            "password": password
+        }
+    }
+
+    send_request(url_to_send, data_to_send);
 }
