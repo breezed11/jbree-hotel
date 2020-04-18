04e4843aa023dfca06f243dd84c3902fb1befdcf
jamesbree99@gmail.com <jamesbree99@gmail.com>
2020-04-18 09:41:25 +0100

    Added types to js request

--- javascript/hotel.js
+++ javascript/hotel.js
@@ -1,9 +1,10 @@
-function send_request(url_to_send, data_to_send) {
+function send_request(url_to_send, data_to_send, type_to_send) {
     $.ajax(url_to_send, {
         type: 'POST',
         data: {
             request: data_to_send,
-            system_ref: document.getElementById("system_ref").value
+            system_ref: document.getElementById("system_ref").value,
+            type: type_to_send
         },
         success: function (result) {
             check_for_errors(result);
@@ -27,9 +28,9 @@ function login() {
             "username": username,
             "password": password
         }
-    }
+    };
 
     data_to_send = JSON.stringify(data_to_send);
 
-    send_request(url_to_send, data_to_send);
+    send_request(url_to_send, data_to_send, 'login');
 }
